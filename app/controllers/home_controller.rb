e46ae30c722bf2b8c4140370009c1e44a4f179bb
Mario Hayashi <mario.hayashi@gmail.com>
2015-01-28 02:25:35 +0000

    Externalise styling properties of sidebar and move profile.json to asset pipeline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    file = File.read(Rails.root.join("public/profile.json"))
+    file = File.read(Rails.root.join("app/assets/config/profile.json"))
     @profile = JSON.parse(file)
   end
 end
